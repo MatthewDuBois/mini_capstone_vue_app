45be138e77442c5163715c6f5a4f27f58f039766
Matthew DuBois <mattrdubois@gmail.com>
2019-02-18 16:39:50 -0600

    end of day

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>New Product</h1>
+    <div>
+      <div>
+        Name: <input v-model="newProductName">
+      </div>
+      <div>
+        Description: <input v-model="newProductDescription">
+      </div>
+      <div>
+        Price: <input v-model="newProductPrice">
+      </div>
+      <div>
+        Image URL: <input v-model="newProductImageUrl">
+      </div>
+      <button v-on:click="createProduct()">Create</button>
+    </div>
+
+    <h1>All Products</h1>
+    <div v-for="product in products">
+    <h2>{{ product.name }}</h2>
+    <img v-bind:src="product.image_url" v-bind:alt="product.name">
+    <p>Price: {{ product.formatted.price }}</p>
+    </div>
   </div>
 </template>
 
+<style>
+  
+  img{
+    width: 200px;
+  }
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  var axios = require('axios');
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      products: [],
+      newProductName: "",
+      newProductDescription: "",
+      newProductPrice: "",
+      newProductImageUrl: ""
+    };
+  },
+  created: function() {
+    axios.get("/api/products")
+      .then(response => {
+        this.products = response.data;
+      });
+  },
+  methods: {
+    createProduct: function() {
+          console.log("Create the Product...");
+          var params = {
+                        name: this.newProductName,
+                        description: this.newProductDescription,
+                        price: this.newProductPrice,
+                        image_url: this.newProductImageUrl
+                        };
+          axios.post("/api/products", params)
+            .then(response => {
+              console.log("Success", response.data);
+              this.products.push(response.data);
+            });
+        }
+      }
+    };
 </script>
+
+
